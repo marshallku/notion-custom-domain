e81ac7ba3a5aaab3443e1f5157bd07b155ebc31d
Marshall Ku <marshall@kakao.com>
2024-04-29 08:52:43 +0900

    Add state with environmental variables in app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use axum::{response::IntoResponse, routing::get, Router};
+mod env;
+
+use axum::{extract::State, response::IntoResponse, routing::get, Router};
 use tokio;
 use tracing::info;
 
-async fn handle_request() -> impl IntoResponse {
-    "Hello, World!".into_response()
+#[derive(Clone)]
+pub struct AppState {
+    host: String,
+    port: u16,
+    address: String,
+    notion_page_url: String,
+}
+
+impl AppState {
+    pub fn from_env() -> Self {
+        let env = env::Env::new();
+
+        Self {
+            host: env.host.into_owned(),
+            port: env.port,
+            address: env.address.into_owned(),
+            notion_page_url: env.notion_page_url.into_owned(),
+        }
+    }
+}
+
+async fn handle_request(State(state): State<AppState>) -> impl IntoResponse {
+    state.notion_page_url.to_string().into_response()
 }
 
 #[tokio::main]
@@ -13,9 +36,11 @@ async fn main() {
         .compact()
         .init();
 
-    let addr = format!("{}:{}", "127.0.0.1", 8099);
-    let app = Router::new().route("/", get(handle_request));
-
+    let state = AppState::from_env();
+    let addr = format!("{}:{}", state.address, state.port);
+    let app = Router::new()
+        .route("/", get(handle_request))
+        .with_state(state);
     let listener = tokio::net::TcpListener::bind(addr.as_str()).await.unwrap();
 
     info!("Server running at http://{}", addr);
