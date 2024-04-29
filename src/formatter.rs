e7bc4803b94c642228a21ce32acd0f611ac3f76b
Marshall Ku <marshall@kakao.com>
2024-04-29 23:38:09 +0900

    Add formatter for injecting script to notion page

--- src/formatter.rs
+++ src/formatter.rs
@@ -2,3 +2,21 @@ pub fn remove_notion_url(body: String) -> String {
     body.replace("https://www.notion.so", "http://localhost:3000")
         .replace("https://notion.so", "http://localhost:3000")
 }
+
+pub fn format_notion_page(body: String) -> String {
+    remove_notion_url(body).replace(
+        "</body>",
+        r#"
+    <script>
+    const observer = new MutationObserver(function() {
+      window.history.replaceState({}, "", "/");
+    });
+    observer.observe(document.getElementById("notion-app"), {
+      childList: true,
+      subtree: true,
+    });
+    </script>
+    </body>
+    "#,
+    )
+}
