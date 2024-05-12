6e8b3970567b55ddb2e553be951ccb18cd11d9b3
Marshall Ku <marshall@kakao.com>
2024-05-12 19:11:22 +0900

    Verify read_file()

--- src/file.rs
+++ src/file.rs
@@ -6,3 +6,26 @@ pub fn read_file(file_path: &str) -> Result<String, std::io::Error> {
     file.read_to_string(&mut contents)?;
     Ok(contents)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use tempfile::tempdir;
+
+    #[tokio::test]
+    async fn test_read_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.txt");
+        let mut file = File::create(file_path.clone())
+            .and_then(|file| Ok(file))
+            .unwrap();
+
+        file.write_all(b"test").unwrap();
+
+        let contents = read_file(file_path.to_str().unwrap()).unwrap();
+
+        assert_eq!(contents, "test");
+    }
+}
