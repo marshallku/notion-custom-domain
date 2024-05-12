1d7023855d516fc572fb0645eeff7a4d8ed03d2c
Marshall Ku <marshall@kakao.com>
2024-05-12 19:11:36 +0900

    Verify http utils

--- src/http.rs
+++ src/http.rs
@@ -37,3 +37,49 @@ pub async fn response_file(file_path: &PathBuf) -> Response {
 
     (get_cache_header(YEAR_TO_SECONDS), body).into_response()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use http_body_util::BodyExt;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use tempfile::tempdir;
+
+    #[tokio::test]
+    async fn test_response_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.txt");
+        let mut file = File::create(file_path.clone())
+            .and_then(|file| Ok(file))
+            .unwrap();
+
+        file.write_all(b"test").unwrap();
+
+        let response = response_file(&file_path).await;
+        let body = response.collect().await.unwrap().to_bytes();
+
+        assert_eq!(body, "test".as_bytes());
+    }
+
+    #[test]
+    fn test_get_cache_header() {
+        let headers = get_cache_header(0);
+        let cache_control = headers.get("Cache-Control").unwrap().to_str().unwrap();
+
+        assert_eq!(cache_control, "no-cache");
+
+        let headers = get_cache_header(100);
+        let cache_control = headers.get("Cache-Control").unwrap().to_str().unwrap();
+
+        assert_eq!(cache_control, "public, max-age=100");
+    }
+
+    #[test]
+    fn test_response_error() {
+        let response = response_error(StatusCode::NOT_FOUND);
+        let status = response.status();
+
+        assert_eq!(status, StatusCode::NOT_FOUND);
+    }
+}
